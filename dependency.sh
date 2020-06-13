da4d9fc1b5f3c8d15daff5e9855f7c0d5960a7dd
XiaoYang <670171961@qq.com>
2020-06-13 21:01:34 +0800

    Auto complete

--- dependency.sh
+++ dependency.sh
@@ -7,29 +7,29 @@ then
 fi
 
 apt upgrade
-apt update
+apt update -y
 
 # basis for the judgehost program
-apt install \
+apt install -y \
     make sudo debootstrap autoconf automake \
     unzip libcgroup-dev lsof procps \
     php-cli php-curl php-json php-xml php-zip \
     libcurl4-gnutls-dev libjsoncpp-dev libmagic-dev
 
 # C/C++
-apt install gcc c++
+apt install -y gcc c++
 
 # Java
-apt install openjdk-8-jre-headless openjdk-8-jdk
+apt install -y openjdk-8-jre-headless openjdk-8-jdk
 
 # Haskell
-apt install ghc
+apt install -y ghc
 
 # Pascal
-apt install fp-compiler
+apt install -y fp-compiler
 
 # Python 2/3
-apt install python3 python2.7
+apt install -y python3 python2.7
 
 # C#
-apt install mono-runtime mono-devel
+apt install -y mono-runtime mono-devel
