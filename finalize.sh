1e4d1c4caa800c0039b60d162f250ee64185860a
XiaoYang <670171961@qq.com>
2021-08-07 14:10:23 +0800

    Update finalize.sh

--- finalize.sh
+++ finalize.sh
@@ -15,8 +15,11 @@ ln -s /opt/domjudge/lib/systemd/system/create-cgroups.service /etc/systemd/syste
 
 # Debootstrap
 echo "Choose debootstrap source"
-echo "[1] azure.archive.ubuntu.com"
-echo "[2] mirrors.cloud.aliyuncs.com"
+echo "[1] azure.archive.ubuntu.com (suggested on azure VM)"
+echo "[2] mirrors.aliyun.com"
+echo "[3] mirrors.cloud.aliyuncs.com (suggested on aliyun ECS)"
+echo "[4] mirrors.tuna.tsinghua.edu.cn"
+echo "[5] cn.archive.ubuntu.com"
 echo "[other] us.archive.ubuntu.com"
 read -p "Input your choice: " choice
 if [[ $choice -eq 1 ]]
@@ -24,7 +27,16 @@ then
     sed -i 's,http://us.archive.ubuntu.com./ubuntu/,http://azure.archive.ubuntu.com/ubuntu,g' /opt/domjudge/judgehost/bin/dj_make_chroot
 elif [[ $choice -eq 2 ]]
 then
+    sed -i 's,http://us.archive.ubuntu.com./ubuntu/,http://mirrors.aliyun.com/ubuntu,g' /opt/domjudge/judgehost/bin/dj_make_chroot
+elif [[ $choice -eq 3 ]]
+then
     sed -i 's,http://us.archive.ubuntu.com./ubuntu/,http://mirrors.cloud.aliyuncs.com/ubuntu,g' /opt/domjudge/judgehost/bin/dj_make_chroot
+elif [[ $choice -eq 4 ]]
+then
+    sed -i 's,http://us.archive.ubuntu.com./ubuntu/,http://mirrors.tuna.tsinghua.edu.cn/ubuntu,g' /opt/domjudge/judgehost/bin/dj_make_chroot
+elif [[ $choice -eq 5 ]]
+then
+    sed -i 's,http://us.archive.ubuntu.com./ubuntu/,http://cn.archive.ubuntu.com/ubuntu,g' /opt/domjudge/judgehost/bin/dj_make_chroot
 fi
 
 /opt/domjudge/judgehost/bin/dj_make_chroot
@@ -35,8 +47,9 @@ sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/GRUB_CMDLINE_LINUX_DEFAULT="quiet cg
 if cat /etc/default/grub | grep "cgroup" > /dev/null
 then
     update-grub
+    echo "Updated /etc/default/grub. If you have other grub.d files, please update them and add 'cgroup_enable=memory swapaccount=1' to them."
+    echo "Please reboot the computer."
 else
     echo "Grub file is not set. Please set up 'cgroup_enable=memory swapaccount=1' in /etc/default/grub manually."
+    echo "After fixing the grub file, please reboot the computer."
 fi
-
-echo "Please reboot the computer."
